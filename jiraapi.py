3a2ffb248514c596581214ed3c46e49f7b696148
Richard Allen <rsaxvc@rsaxvc.net>
2014-01-11 12:32:55 -0600

    just a bit of API doc

--- jiraapi.py
+++ jiraapi.py
@@ -1,14 +1,17 @@
 class JiraAPI:
+	"""implements a connection to a JIRA server"""
 	def __init__(self, url ):
 		self.url = url
 
 	class JiraLinkType:
+		"""connects this issue to another"""
 		def __init__( self, linktype, linkissue ):
 			self.type=linktype
 			self.issue=linkissue
 		pass
 
 	class JiraIssueType:
+		"""represents a single JIRA issue"""
 		def __init__( self, issuekey, issuelinks ):
 			self.key = issuekey
 			self.links = issuelinks
@@ -17,7 +20,10 @@ class JiraAPI:
 	import json
 
 	def fetchIssuesFromProject( self, projectname ):
+		"""given a project name, returns a list of issues in that project"""
 		pass
+
 	def fetchIssue( self, issuename ):
+		"""given an issue name(ISSUE-n), returns that issue"""
 		pass
 	pass
