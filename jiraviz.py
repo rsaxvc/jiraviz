e06619d326f0c43be05017e4ddf103f780aa6072
Richard Allen <rsaxvc@rsaxvc.net>
2014-01-12 10:06:16 -0600

    Add command-line arguments, leave old setup as default/demo

--- jiraviz.py
+++ jiraviz.py
@@ -2,27 +2,34 @@
 
 #get all the arguments with argparse
 import argparse
+parser = argparse.ArgumentParser(description='Scrape JIRA issue dependencies and graph them')
+parser.add_argument("--api", help="API target, like http://jira.atlassian.com (no trailing slash)")
+parser.add_argument("--entrypoint", help="Entry point, comma-separated list of JIRA issues or projects")
+parser.add_argument("--username", help="username of scraper user")
+parser.add_argument("--password", help="password of dedicated scraper user")
+args = parser.parse_args()
+
+if not args.api:
+	args.api = "http://jira.atlassian.com"
+	print "using default API server:",args.api
+
+if not args.entrypoint:
+	args.entrypoint = "CWD-3051,WBS-4"
+	print "using demo API entrypoints:",args.entrypoint
+
+#still need to add
 	#output image type
-	#API url
-	#jira-project or jira-issue
-	#username?
-	#password?
 	#optional-goal-issue
 	#colors?
 	#output-filename?
 	#output-resolution?
 	#max-fetch-glob - in case of angry jira server admin
 
-api_url = "http://jira.atlassian.com"
-entry_point = "CWD-3051,WBS-4"
-username = None
-password = None
-
 #get all the data with JiraWalk
 from jirawalk import JiraWalk
 
 #start with project or user-selected issue
-j = JiraWalk(api_url, entry_point, username, password)
+j = JiraWalk(args.api, args.entrypoint, args.username, args.password)
 
 #graph the graph with graphviz
 import pydot	
@@ -40,4 +47,4 @@ for issue in j.nodes:
 for edge in j.edges:
 	graph.add_edge( pydot.Edge(nodes[edge.tail], nodes[edge.head]) )
 
-graph.write_svg(entry_point + '.svg')
+graph.write_svg(args.entrypoint + '.svg')
