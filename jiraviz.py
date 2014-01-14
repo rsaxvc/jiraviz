83670609616ab3974f2c38bea1d260e7fdbdd2d6
Richard Allen <rsaxvc@rsaxvc.net>
2014-01-13 23:51:13 -0600

    Display priority as well

--- jiraviz.py
+++ jiraviz.py
@@ -55,7 +55,7 @@ for issuekey in j.nodes:
 				break
 
 	nodes[issue.key] = pydot.Node(
-		(issue.key+"("+issue.status+")\\n"+issue.summary).replace("\"","\\\""),
+		(issue.key+"("+issue.status+")["+issue.priority+"]\\n"+issue.summary).replace("\"","\\\""),
 		style="filled",
 		URL="\"" + args.api + "/browse/" + issue.key + "\"",
 		color=color
